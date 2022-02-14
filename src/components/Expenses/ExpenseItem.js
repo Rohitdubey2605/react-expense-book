eff34b849de4a23cc742f828bad7f90edc9b39cc
Rohit Dubey <Rohit.Dubey@odessainc.com>
2022-02-14 22:38:12 +0530

    ExpenseItem Updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [amount, setAmount] = useState(props.amount);
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setAmount(0);
+    setTitle("No Title Available");
+  };
+
   return (
     <Card className="expense-item">
       <div>
         <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Reset Title and Amount</button>
     </Card>
   );
 }
